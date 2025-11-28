edbda559a7cd5090dc025c6fb79d84c25bdebe0e
yoha <sushiyoha@proton.me>
2025-11-28 20:14:25 +0800

    1

--- src/main.tsx
+++ src/main.tsx
@@ -23,6 +23,14 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   // </React.StrictMode>
 );
 
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  // ✨ 唯一的、最后的魔法就在这里！✨
+  // ✨ 告诉 React Router，我们的家在 /word-matching！✨
+  <BrowserRouter basename="/word-matching">
+    <App />
+  </BrowserRouter>
+);
+
 // 注册 Service Worker
 // if ("serviceWorker" in navigator) {
 //   window.addEventListener("load", () => {
